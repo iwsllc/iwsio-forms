71bab12ef589dbc4f4b9bb79aebc457c3a709b3c
Nathan Bridgewater <nathan@iws.io>
2023-06-18 09:06:40 -0500

    Adding jsdoc.

--- forms/src/utils.ts
+++ forms/src/utils.ts
@@ -1,3 +1,9 @@
+/**
+ * Return a new shallow copied array/object with values/keys ommitted that match the provided check.
+ * @param values Source values/keys
+ * @param omitWhen Excludes values that return true
+ * @returns Shallow copied object or array without values/keys ommitted.
+ */
 export const omitBy = (values: Record<string, any> | any[], omitWhen: (v: any) => boolean): Record<string, any> | any[] => {
 	if (Array.isArray(values)) return values.filter((v) => !omitWhen(v))
 
