50f620303f48ca3d4e74b02ff71a95041ce2eb69
Nathan Bridgewater <nathan@iws.io>
2024-03-03 14:49:42 -0600

    4.0 Refactoring useFieldState args, onValidSubmit, including new field state props for isFormBusy, toggleFormBusy for managing custom styling/DOM state.

--- forms/src/FieldManagerForm.tsx
+++ forms/src/FieldManagerForm.tsx
@@ -1,18 +1,34 @@
 import { forwardRef } from 'react'
 import { useFieldManager } from './useFieldManager'
 import { ValidatedForm, ValidatedFormProps } from './ValidatedForm'
+import { FieldValues } from './types'
 
-export type ManagedValidatedFormProps = Omit<ValidatedFormProps, 'onValidSubmit' | 'noValidate'>
+export type ManagedValidatedFormProps = Omit<ValidatedFormProps, 'onValidSubmit' | 'noValidate'> & {
+	/**
+	 * Callback to be called when form is valid and submitted. Provides current field values.
+	 */
+	onValidSubmit?: (fields: FieldValues) => void
+	/**
+	 * If true, the form will be set to busy after a valid submit event is triggered and wait for `toggleBusy` to be called with a value of `false` to reset.
+	 * This is useful for forms that need to wait for a server response before allowing another submit.
+	 * Default value: false
+	 */
+	holdBusyAfterSubmit?: boolean
+}
 
-export const FieldManagerForm = forwardRef<HTMLFormElement, ManagedValidatedFormProps>(({ children, ...props }, ref) => {
-	const { fields, onValidSubmit, setReportValidation } = useFieldManager()
+export const FieldManagerForm = forwardRef<HTMLFormElement, ManagedValidatedFormProps>(({ children, onValidSubmit, holdBusyAfterSubmit, ...props }, ref) => {
+	const { fields, setReportValidation, toggleFormBusy } = useFieldManager()
 	const handleLocalSubmit = () => {
 		setReportValidation(true)
 	}
+	// no args; this comes from ValidatedForm
 	const handleLocalValidSubmit = () => {
+		toggleFormBusy(true)
 		if (onValidSubmit != null) {
 			onValidSubmit(fields)
 		}
+		if (holdBusyAfterSubmit) return
+		toggleFormBusy(false)
 	}
 	return <ValidatedForm ref={ref} {...props} onValidSubmit={handleLocalValidSubmit} onSubmit={handleLocalSubmit}>{children}</ValidatedForm>
 })
